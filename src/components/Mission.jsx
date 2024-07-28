95ae7998502bf4d8add2f5e4b0082761082f3cd4
Lalitzzz <armyofficer01@gmail.com>
2024-07-28 18:37:06 +0530

    first commit mine

---
+++ src/components/Mission.jsx
@@ -0,0 +1,39 @@
+import missionImg from "../assets/mission.jpeg";
+import mission from "../assets/mission.mp4";
+import { MISSION } from "../constants";
+import { motion  } from "framer-motion";
+const Mission = () => {
+  return (
+     <section id="mission" >
+      <div className="container mx-auto text-center">
+        <h2 className="mb-8 text-3xl lg:text-4xl"> Our Mission</h2>
+         <div className="relative flex items-center justify-center">
+            <motion.video className="w-full rounded-3xl " 
+             initial={{opacity:0}}
+             whileInView={{opacity:1}}
+             viewport={{once:true}}
+             transition={{duration:1}}
+            autoPlay muted loop playsInline poster={missionImg}>
+               <source src={mission} type="video/mp4" />
+            </motion.video>
+             <motion.div className="absolute h-full w-full rounded-3xl bg-black/40 "
+              initial={{opacity:0}}
+              whileInView={{opacity:1}}
+              viewport={{once:true}}
+              transition={{duration:0.5, delay:0.5}}
+             />
+             <motion.p className="absolute max-w-lg tracking-tighter lg:text-3xl"
+              initial={{opacity:0}}
+              whileInView={{opacity:1, y:0}}
+              viewport={{once:true}}
+              transition={{duration:1 , delay:0.5}}
+             >
+                {MISSION}
+             </motion.p>
+         </div>
+      </div>
+     </section>
+  )
+}
+
+export default Mission
